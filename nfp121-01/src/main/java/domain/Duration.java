f907d24f9c0a380ce17d6a0bc7b525560c1b9330
Nathannicolle <nathan-nicolle@orange.fr>
2023-03-05 22:59:49 +0100

    ajout des méthodes oneMinuteLater, oneMinuteEarlier et toString

--- nfp121-01/src/main/java/domain/Duration.java
+++ nfp121-01/src/main/java/domain/Duration.java
@@ -1,7 +1,23 @@
 package domain;
 
 public class Duration {
+    private int duration;
     public Duration() {
 
+    }
+
+    public void oneMinuteLater() {
+        duration++;
+    }
+
+    public void oneMinuteEarlier() {
+        if(duration > 0) {
+            duration--;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Durée : " + this.duration;
     }
 }
